9e5a996336f4fc1c000fb8d06b98fbf3c952d109
cleugley <cleugley@fullsail.edu>
2015-04-30 08:47:33 -0600

    Analyze Object.person assignment

---
+++ WK4/Analyze OBJECTS.Person/js/main.js
@@ -0,0 +1,23 @@
+// Crystal Eugley
+// 4/29/2015
+// ANALYZE! OBJECTS.Person Week 4
+
+/* PSEUDO-CODE */
+
+
+/*Create and array
+    In the array include 5 names
+
+Next create a for loop
+    In the for loop add Math.random() so that the five names from the
+    above array will be randomly selected when the for loop runs.
+
+    Create a function() called populateHTML
+        This function will populate the selected name from the array on the
+        webpage.
+
+    Create the function so it will not display duplicated names.
+
+    Set up a runUpdate() function
+        Run this code 30 times per second
+ */
---
+++ WK4/Analyze OBJECTS.Person/js/person.js
@@ -0,0 +1,19 @@
+// Crystal Eugley
+// 4/29/2015
+// ANALYZE OBJECTS.Person
+
+/* PSUEDO - CODE FOR PERSON.JS
+
+Add Person object to go with main.js file
+
+The Person object needs for constructors
+    name    This is the name of the person
+    action  States what the person is actively doing
+    job     set from the jobs array
+    row
+
+    Set up update()function
+        This will change the action of the person every so often.
+        Should be set every 5 seconds or so and be displayed in HTML
+        column 3
+ */
